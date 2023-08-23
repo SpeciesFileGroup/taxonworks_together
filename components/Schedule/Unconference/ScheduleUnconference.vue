651e95899c5e1d549500b5654ff5c69ef40921ad
jlpereira <pepelui@outlook.com>
2023-08-23 15:14:26 -0300

    Change css style

--- components/Schedule/Unconference/ScheduleUnconference.vue
+++ components/Schedule/Unconference/ScheduleUnconference.vue
@@ -11,10 +11,10 @@
       class="text-gray-500 text-sm"
       v-html="item.description"
     />
-    <div class="flex flex-col gap-2 flex-shrink-2 my-4">
+    <div class="flex flex-row gap-2 flex-shrink-2 my-4">
       <div
         v-for="topic in item.topics"
-        class="border rounded-md border-secondary p-4"
+        class="border rounded-md border-secondary p-2"
       >
         {{ topic }}
       </div>

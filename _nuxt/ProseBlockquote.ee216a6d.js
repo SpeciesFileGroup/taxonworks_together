0ede7dc2c6063030fbf4e8f09e0e001ee6e50ca9
jlpereira <jlpereira@users.noreply.github.com>
2023-08-25 13:40:25 +0000

    deploy: cc321b89d166fde12377438968a7c54e28d90413

--- _nuxt/ProseBlockquote.ee216a6d.js
+++
@@ -1,1 +0,0 @@
-import{a as o,o as t,b as r,y as c}from"./entry.160980c7.js";const s={};function n(e,a){return t(),r("blockquote",null,[c(e.$slots,"default")])}const f=o(s,[["render",n]]);export{f as default};

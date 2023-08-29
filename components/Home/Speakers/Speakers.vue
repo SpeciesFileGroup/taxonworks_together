2c360e768bee89cbc0e55d9547adca55a6603d31
jlpereira <pepelui@outlook.com>
2023-08-29 14:01:42 -0300

    Update speakers section

--- components/Home/Speakers/Speakers.vue
+++ components/Home/Speakers/Speakers.vue
@@ -8,22 +8,11 @@
         Speakers
       </h1>
       <div class="flex flex-row flex-wrap justify-around gap-8">
-        <div
+        <HomeSpeakersBox
           v-for="item in data"
-          class="flex flex-col items-center"
-        >
-          <img
-            class="w-40 h-40 rounded-full object-cover"
-            :src="item.img"
-            :alt="item.name"
-          />
-          <div
-            class="relative bottom-4 bg-secondary w-44 text-white text-center p-2 flex flex-col rounded-md"
-          >
-            <span>{{ item.name }}</span>
-            <span class="text-xs mt-1">{{ item.description }}</span>
-          </div>
-        </div>
+          :key="item.name"
+          :speaker="item"
+        />
       </div>
     </div>
   </section>
---
+++ components/Home/Speakers/SpeakersBox.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="flex flex-col items-center">
+    <img
+      v-if="speaker.img"
+      class="w-40 h-40 rounded-full object-cover"
+      :src="speaker.img"
+      :alt="speaker.name"
+    />
+    <div
+      class="w-40 h-40 rounded-full bg-secondary-dark flex items-center justify-center"
+      v-else
+    >
+      <IconPerson class="w-20 h-20 stroke-primary" />
+    </div>
+    <div
+      class="relative bottom-4 bg-secondary w-44 text-white text-center p-2 flex flex-col rounded-md min-h-[80px]"
+    >
+      <span class="font-bold">{{ speaker.name }}</span>
+      <span class="text-xs mt-1">{{ speaker.description }}</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface IProps {
+  speaker: {
+    name: string
+    img?: string
+    description: string
+  }
+}
+
+defineProps<IProps>()
+</script>

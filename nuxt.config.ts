6f806f124286b4e1055f7bf5a81cca88cdf09f60
jlpereira <pepelui@outlook.com>
2024-11-08 23:29:29 -0300

    Update base URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/2024'
+  },
   modules: ['@nuxtjs/google-fonts', '@nuxt/content', '@nuxtjs/tailwindcss'],
   googleFonts: {
     families: {

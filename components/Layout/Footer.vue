2ffd1c816e38f8c84db729f67a74f4b1211d9a4e
jlpereira <pepelui@outlook.com>
2025-02-13 18:06:07 -0300

    Add SFG logo

--- components/Layout/Footer.vue
+++ components/Layout/Footer.vue
@@ -1,47 +1,60 @@
 <template>
   <footer class="w-full bg-secondary-dark text-white">
-    <div class="container p-8 mx-auto flex justify-between">
-      <div class="flex flex-col justify-between">
-        <span class="text-lg font-medium">© Species File Group</span>
-        <a
-          class="text-primary"
-          href="https://github.com/SpeciesFileGroup/taxonworks_together"
-          >GitHub Repository</a
-        >
+    <div
+      class="container p-8 mx-auto flex md:flex-row justify-between gap-12 flex-col-reverse"
+    >
+      <div class="flex flex-col">
+        <a href="https://speciesfilegroup.org">
+          <img
+            src="~/assets/images/sfg_logo.svg"
+            class="w-20"
+          />
+        </a>
       </div>
-      <div>
-        <div class="mb-2 text-lg font-bold">Social</div>
-        <ul>
-          <li
-            v-for="item in social"
-            :key="item.link"
-          >
-            <a
-              class="text-primary"
-              :href="item.link"
+      <div class="flex flex-col md:flex-row gap-6 md:gap-24">
+        <div v-for="(links, section) in links">
+          <div class="mb-2 text-lg font-bold capitalize">{{ section }}</div>
+          <ul>
+            <li
+              v-for="item in links"
+              :key="item.url"
+              class="my-2"
             >
-              {{ item.title }}
-            </a>
-          </li>
-        </ul>
+              <a
+                class="text-primary"
+                :href="item.url"
+              >
+                {{ item.title }}
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </footer>
 </template>
 
 <script setup>
-const social = [
-  {
-    title: 'TaxonWorks Mastodon',
-    link: 'https://fosstodon.org/@TaxonWorks'
-  },
-  {
-    title: 'TaxonWorks YouTube',
-    link: 'https://www.youtube.com/@TaxonWorks/'
-  },
-  {
-    title: 'Species File Group blog',
-    link: 'http://speciesfilegroup.org/blog.html'
-  }
-]
+const links = {
+  code: [
+    {
+      title: 'GitHub Repository',
+      url: 'https://github.com/SpeciesFileGroup/taxonworks_together'
+    }
+  ],
+  social: [
+    {
+      title: 'TaxonWorks Mastodon',
+      url: 'https://fosstodon.org/@TaxonWorks'
+    },
+    {
+      title: 'TaxonWorks YouTube',
+      url: 'https://www.youtube.com/@TaxonWorks/'
+    },
+    {
+      title: 'Species File Group blog',
+      url: 'http://speciesfilegroup.org/blog.html'
+    }
+  ]
+}
 </script>

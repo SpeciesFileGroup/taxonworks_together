f3bae7b6395ce15ac7215e98bc0092a390959c7a
jlpereira <pepelui@outlook.com>
2025-05-07 13:32:22 -0300

    Add awards

---
+++ components/Icon/IconChart.global.vue
@@ -0,0 +1,26 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 50 50"
+  >
+    <g
+      fill="none"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="3"
+    >
+      <path
+        stroke="currentColor"
+        d="M6.25 25c8.333 0 27.083-4.167 35.417-18.75"
+      />
+      <path
+        stroke="currentColor"
+        d="M33.333 6.25h8.334l2.083 8.333"
+      />
+      <path
+        stroke="currentColor"
+        d="M12.5 43.75H6.25v-8.333h6.25zm16.667-12.5h-6.25v12.5h6.25zm16.666-8.333h-6.25V43.75h6.25z"
+      />
+    </g>
+  </svg>
+</template>

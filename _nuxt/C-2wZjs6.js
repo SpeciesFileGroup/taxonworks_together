f90d44abfaae3ca3c193f25cb92b89459f56bf96
mjy <mjy@users.noreply.github.com>
2026-05-18 15:33:06 +0000

    deploy: b758de93dd8d4bbbc2da92eba84ab723482dbd6b

---
+++ _nuxt/C-2wZjs6.js
@@ -0,0 +1,1 @@
+import{_ as s,c as o,H as r,o as t}from"./BL1FfSaB.js";const n={};function c(e,a){return t(),o("th",null,[r(e.$slots,"default")])}const l=Object.assign(s(n,[["render",c]]),{__name:"ProseTh"});export{l as default};
